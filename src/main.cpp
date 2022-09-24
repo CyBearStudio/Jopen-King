31126f259941304579d719909cfa3e2aec34e381
Timothée LY <tim0thee.ly@free.fr>
2022-09-24 23:45:05 +0200

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
 
   // Code adapted from the SFML 2 "Window" example.
 
-  //cout << "Version " << myproject_VERSION_MAJOR << "." << myproject_VERSION_MINOR << endl;
+  cout << "Version " << myproject_VERSION_MAJOR << "." << myproject_VERSION_MINOR << endl;
 
   sf::Window App(sf::VideoMode(800, 600), "myproject");
 
