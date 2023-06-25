8c546f94b4dde265876d788003fb130470f87a8c
Timothée LY <tim0thee.ly@free.fr>
2023-06-25 18:32:51 +0200

    more states and adding the spritesheet to the repo

--- include/Character.h
+++ include/Character.h
@@ -5,16 +5,14 @@
 
 class Character : public sf::Drawable, public sf::Transformable {
 
-    enum State{Idle, Running_left, Running_right, Jumping, Charging_jump, Collision};
+    enum State{Idle, Running_left, Running_right, Jumping, Charging_jump, Collision, Falling, Land, Dead};
 
 public:
     Character(sf::Vector2f pos = sf::Vector2f(0.f,0.f));
 
-
 private:
 
     State curr_state;
-
 };
 
 
